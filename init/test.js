fff9637fe92de4c4c379f26e3b87d6458cba157a
John Zakkam <42964266+imjohnzakkam@users.noreply.github.com>
2020-03-01 12:54:03 +0530

    Update test.js

--- init/test.js
+++ init/test.js
@@ -1,1 +1,14 @@
-#hi
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Test</h2>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 'John Zakkam';
+</script>
+
+</body>
+</html>
